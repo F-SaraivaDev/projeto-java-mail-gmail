6b3a3c4b59732047bb11593f609ad872272c14c8
F-SaraivaDev <saraiva@gmail.com>
2022-01-31 11:17:52 -0300

    Projeto Enviando Email pelo Java

---
+++ src/main/java/enviando_mail/enviando_mail/App.java
@@ -0,0 +1,13 @@
+package enviando_mail.enviando_mail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
