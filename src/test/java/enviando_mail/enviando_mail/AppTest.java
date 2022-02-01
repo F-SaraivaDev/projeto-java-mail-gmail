a9ccae3b819f952bc9b4d04aa677d334b3b8be5d
F-SaraivaDev <saraivafds@gmail.com>
2022-01-31 23:19:10 -0300

    Criando a classe ObjetoEnviaEmail para reutilizar

--- src/test/java/enviando_mail/enviando_mail/AppTest.java
+++ src/test/java/enviando_mail/enviando_mail/AppTest.java
@@ -18,47 +18,17 @@ import org.junit.Test;
  */
 public class AppTest{
 	
-	private String userName = "testejavamailjavaweb@gmail.com";
-	private String senha = "skyneT91";
+	
 		
 	@Test
-	public void testeEmail() {
+	public void testeEmail() throws Exception {
 		
-		try {
-			Properties properties = new Properties();
-			properties.put("mail.smtp.auth", "true");
-			properties.put("mail.smtp.starttls", "true");
-			properties.put("mail.smtp.host", "smtp.gmail.com");
-			properties.put("mail.smtp.port", 465);
-			properties.put("mail.smtp.socketFactory.port", "465");
-			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			
-			Session session = Session.getInstance(properties, new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					// TODO Auto-generated method stub
-					return new PasswordAuthentication(userName, senha);
-				}
-			});
-			
-			Address[] toUser = InternetAddress.parse("testejavamailjavaweb@gmail.com,"
-			                       +"saraivafds@gmail.com, fersaraiva2018@gmail.com" );
-			
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(userName));
-			message.setRecipients(Message.RecipientType.TO, toUser);
-			message.setSubject("Chegou o e-mail enviado com Java");
-			message.setText("Olá programador, vc acaba de receber um e-mail enviado com java"
-			              + "do curso JDEV desenvolvimento");
-			
-			Transport.send(message);
-			
-		} catch (Exception e) {
-            e.printStackTrace();
-		}
+		ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail("saraivafds@gmail.com", 
+				"Fernando Saraiva", 
+				"Teste Envia E-mail Java", 
+				"Treinamento envia e-mail com java");
 		
-		
-		
+		enviaEmail.enviarEmail();
 	}
 
 }    
